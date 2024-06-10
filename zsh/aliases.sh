e83657a5376affd1344a4d61595784f63c668958
Salman Ahmed <git@salman.so>
2024-06-10 06:29:31 +0300

    Update

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -8,5 +8,5 @@ alias cfg="nvim ~/.config"
 alias cfgnvim="nvim ~/.config/nvim"
 alias cfgkar="nvim ~/.config/karabiner/commands.json"
 alias lg=lazygit
-alias lgconfig="lazygit --path ~/.config"
+alias ld=lazydocker
 alias brewdump="brew bundle --file $HOME/.config/homebrew/Brewfile dump --force"
